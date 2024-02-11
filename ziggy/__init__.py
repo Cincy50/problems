1d73b221474b71fb03af00af628786688d6615f9
Marcus T <mtwyford@users.noreply.github.com>
2024-02-11 11:01:02 -0500

    Update __init__.py

--- ziggy/__init__.py
+++ ziggy/__init__.py
@@ -19,7 +19,7 @@ def callAyZiggy():
 @check50.check(compiles)
 def callAyZiggyZoomba():
     """Call Out to Ay Ziggy Zoomba Zoomba Zoomba"""
-    check50.run("./ziggy").stdin("Ay Ziggy Zoomba Zoomba Zoomba").stdout("Ay Ziggy Zoomba Zoomba Ze!\n").exit(0)
+    check50.run("./ziggy").stdin("Ay Ziggy Zoomba Zoomba Zoomba").stdout("Ay Ziggy Zoomba Zoomba Ze!\n").stdin("q").exit(0)
 
 @check50.check(compiles)
 def callZiggyQuits():
