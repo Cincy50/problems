1fd0d2f49ec33b095fabe217e9718a60be9faae5
Marcus T <mtwyford@users.noreply.github.com>
2025-10-28 21:12:24 -0400

    Update __init__.py

--- barley/__init__.py
+++ barley/__init__.py
@@ -10,3 +10,8 @@ def exists():
 def compiles():
     """barley.c compiles"""
     check50.c.compile("barley.c", lcs50=True)
+
+@check50.check(compiles)
+def test1():
+    """handles a guess word quint"""
+    check50.run("./barley").stdin("quint").stdout("Your guess is not in my list").exit(0)
