8d467b0e8a00d26e94ffadcdada387befe2a4479
Marcus T <mtwyford@users.noreply.github.com>
2025-11-17 16:22:55 -0500

    Update __inti__.py

--- sorts/bubble/__inti__.py
+++ sorts/bubble/__inti__.py
@@ -1,1 +1,17 @@
-#test
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """sort.c exist"""
+    check50.exists("sort.c")
+
+@check50.check(exists)
+def compiles():
+    """sort.c compiles"""
+    check50.c.compile("sort.c", lcs50=True)
+
+@check50.check(compiles)
+def test1():
+    """handles a static array of ints"""
+    check50.run("./sort").stdout("12345678").exit(0)
